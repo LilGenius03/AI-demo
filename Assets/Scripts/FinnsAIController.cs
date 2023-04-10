09b0a456442bd38bd0954076d245332f1c07ad3d
LilGenius03 <C22486512@mytudublin.ie>
2023-04-10 17:39:37 +0100

    Added alot to our project

---
+++ Assets/Scripts/FinnsAIController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FinnsAIController : MonoBehaviour
+{
+
+    private NavMeshAgent _navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit hit))
+            {
+                SetAITargetLocation(hit.point);
+            }
+            
+        }
+    }
+
+    private void SetAITargetLocation(Vector3 targetLocation)
+    {
+        _navMeshAgent.SetDestination(targetLocation);
+    }
+
+}
