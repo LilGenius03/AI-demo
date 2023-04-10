83aecafbd466e7497f73c5c6031b869de74b4020
LilGenius03 <C22486512@mytudublin.ie>
2023-04-10 20:13:36 +0100

    I made a maze

--- Assets/Scripts/FinnsAIController.cs
+++ Assets/Scripts/FinnsAIController.cs
@@ -5,8 +5,8 @@ using UnityEngine.AI;
 
 public class FinnsAIController : MonoBehaviour
 {
-
     private NavMeshAgent _navMeshAgent;
+    public float timer, wanderTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,11 +25,25 @@ public class FinnsAIController : MonoBehaviour
             }
             
         }
+
+        timer += Time.deltaTime;
     }
 
     private void SetAITargetLocation(Vector3 targetLocation)
     {
         _navMeshAgent.SetDestination(targetLocation);
+    }
+
+    private void Wander()
+    {
+        if(timer >= wanderTime)
+        {
+            Vector2 wanderTarget = Random.insideUnitCircle * wanderTime;
+            Vector3 wanderPos3D = new Vector3(transform.position.x + wanderTarget.x, transform.position.y, transform.position.z + wanderTarget.y);
+            SetAITargetLocation(wanderPos3D);
+            timer = 0;
+        }
+        
     }
 
 }
