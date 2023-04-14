cd2676a2b6bf9b84626c055fe70f82b683e2b21a
LilGenius03 <C22486512@mytudublin.ie>
2023-04-14 16:52:28 +0100

    Added an End Trigger and also Rigidbodies to our characters so they can trigger the end scene don't worry though they are set to kinematic so they aren't affected by gravity

--- Assets/Scripts/EndTrigger.cs
+++ Assets/Scripts/EndTrigger.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void EndGame()
     {
+        SceneManager.LoadScene("PlayerWins");
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Lose()
     {
-        
+        SceneManager.LoadScene("AIWins");
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            EndGame();
+        }
+
+        if(other.CompareTag("AI"))
+        {
+            Lose();
+        }
+
     }
 }
